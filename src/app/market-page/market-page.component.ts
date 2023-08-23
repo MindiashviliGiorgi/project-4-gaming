89a48c1b93d309b16d89789490345a439e2e840d
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-08-24 00:16:43 +0400

    bugs

--- src/app/market-page/market-page.component.ts
+++ src/app/market-page/market-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { MarketPageService } from './market-page.service';
 import { Product } from '../types/product';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-market-page',
@@ -9,12 +10,27 @@ import { Product } from '../types/product';
 })
 export class MarketPageComponent {
 
-  constructor(private marketService : MarketPageService){}
+  constructor(
+    private marketService : MarketPageService, 
+    private authService : AuthService
+    ){}
 
   product : Product[] = [];
+
+  isAuthenticated(){
+    return this.authService.isAuthenticated;
+  }
 
   ngOnInit(): void {
     this.product = this.marketService.getProduct();
   }
+
+  searchValue : string = '';
+  
+  searchTextChange : EventEmitter<string> = new EventEmitter<string>();
+  searchItem(){
+    this.searchTextChange.emit(this.searchValue)
+  }
+
 
 }
